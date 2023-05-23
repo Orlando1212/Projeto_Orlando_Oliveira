b0ab563eed95878d47f929278850027af4686ed9
Orlando1212 <orlandomv19@gmail.com>
2023-05-23 20:20:18 -0300

    Criacao da Classe C e Criado Metodo na Classe A

--- projeto_es.py
+++ projeto_es.py
@@ -2,15 +2,27 @@ class A():
   A1 = 2
   A2 = 2.2
   def MA1():
-    print(MA1))
+    print(MA1())
   def MA2():
-    print(MA2))
+    print(MA2())
+    
+  def MA3():
+    print("Alteração a classe A partir do clone")
     
 class B():
   B1 = 3
   B2 = 3.5
   
   def MB1():
-    print(MB1))
+    print(MB1())
   def MB2():
     print(MB2())
+
+class C():
+  C1 = ""
+  C2 = 4
+  
+  def MC1():
+    print(MC1())
+  def MC2():
+    print(MC2())
