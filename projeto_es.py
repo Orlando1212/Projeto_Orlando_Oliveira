e3c808fcf76d36e66c61597e345f108da93f04a9
Orlando <67079009+Orlando1212@users.noreply.github.com>
2023-05-23 20:09:01 -0300

    Create projeto_es.py

---
+++ projeto_es.py
@@ -0,0 +1,16 @@
+class A():
+  A1 = 2
+  A2 = 2.2
+  def MA1():
+    print(MA1))
+  def MA2():
+    print(MA2))
+    
+class B():
+  B1 = 3
+  B2 = 3.5
+  
+  def MB1():
+    print(MB1))
+  def MB2():
+    print(MB2())
